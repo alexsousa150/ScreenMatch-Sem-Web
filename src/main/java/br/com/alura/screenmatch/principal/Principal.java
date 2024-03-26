8693d76bff4b7a0a6ea2beac70a8d6329b15d9e6
Alex <alexsousa150@gmail.com>
2024-03-26 16:54:59 -0300

    Selecionando os 5 melhores episodios de uma série

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,13 +1,16 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadoEpisodio;
 import br.com.alura.screenmatch.model.DadoSerie;
 import br.com.alura.screenmatch.model.DadoTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverterDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -31,5 +34,22 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadoEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+        temporadas.forEach(t ->t.episodios().forEach(e-> System.out.println(e.titulo())));
+
+        List<DadoEpisodio> dadoEpisodios =temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 Episodios");
+        dadoEpisodios.stream()
+                .sorted(Comparator.comparing(DadoEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
